77aacef6d383503ff53c146f275733b99f0a7561
Nick42-beep <nichlekang@gmail.com>
2023-07-24 18:00:06 -0700

    added stack and queue method headers

---
+++ src/Queue.java
@@ -0,0 +1,26 @@
+public class Queue extends SinglyLinkedList{
+	
+	public Queue(int count, LinkNode start, LinkNode end) {
+		super(count, start, end);
+	}
+	
+	public void enQueue(Currency obj) {
+		
+	}
+	
+	public Currency deQueue() {
+		
+	}
+	
+	public Currency peekFront() {
+		
+	}
+	
+	public Currency peekRear() {
+		
+	}
+	
+	public void printQueue() {
+		
+	}
+}
---
+++ src/Stack.java
@@ -0,0 +1,22 @@
+public class Stack extends SinglyLinkedList{
+    
+	public Stack(int count, LinkNode start, LinkNode end) {
+        super(count, start, end);
+    }
+	
+	public void push(Currency obj) {
+		
+	}
+	
+	public Currency pop(Currency obj) {
+		
+	}
+	
+	public Currency peek() {
+		
+	}
+	
+	public void printStack() {
+		
+	}
+}
