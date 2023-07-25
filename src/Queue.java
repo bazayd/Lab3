9f2626d5b4e3043e225addab5e2f0d36f64e0968
Brandon Arriaga <brandonzarriaga@gmail.com>
2023-07-25 13:05:23 -0700

    Added code to methods in Queue class.

--- src/Queue.java
+++ src/Queue.java
@@ -1,26 +1,67 @@
 public class Queue extends SinglyLinkedList{
+
+
 	
 	public Queue(int count, LinkNode start, LinkNode end) {
 		super(count, start, end);
 	}
 	
 	public void enQueue(Currency obj) {
-		
+
+		LinkNode front = getStart();
+		LinkNode rear = getEnd();
+		LinkNode next = rear.getNext();
+		int count = getCount();
+
+		LinkNode temp = new LinkNode(obj);
+
+		if (front == null) {
+			rear = temp;
+			front = rear;
+		}else {
+			next = temp;
+			rear = next;
+		}
+		++count;
 	}
 	
 	public Currency deQueue() {
+		LinkNode front = getStart();
+		LinkNode rear = getEnd();
+		int count = getCount();
+
+
+		if(front != null) {
+			Currency removed = front.getData();
+			front = front.getNext();
+			--count;
+			return removed;
+		}
 		return null;
-		
 	}
 	
 	public Currency peekFront() {
+
+		LinkNode front = getStart();
+		Currency currency = front.getData();
+
+		if (currency != null) {
+			return currency;
+		}
 		return null;
 		
 	}
 	
 	public Currency peekRear() {
+
+		LinkNode rear = getEnd();
+		Currency currency = rear.getData();
+
+		if (currency != null) {
+			return currency;
+		}
+
 		return null;
-		
 	}
 	
 	public void printQueue() {
