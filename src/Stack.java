441d39326a28bfa7172e7e7fd00b31101caf1737
Nick42-beep <nichlekang@gmail.com>
2023-07-24 21:00:12 -0700

    edited stack.java

--- src/Stack.java
+++ src/Stack.java
@@ -1,24 +1,43 @@
 public class Stack extends SinglyLinkedList{
     
+	/*
+	 * end is top of stack
+	 * start is bottom of stack
+	 */
 	public Stack(int count, LinkNode start, LinkNode end) {
         super(count, start, end);
     }
 	
 	public void push(Currency obj) {
+		/*
+		 * increase object count
+		 * create new node
+		 * newNode.next = null
+		 * new node.data = obj data
+		 */
+		this.setCount(this.getCount() + 1);
+		//LinkNode newNode = new LinkNode(obj.getData(), obj.getNext());
+		//newNode.setData() = obj.getData();
 		
 	}
 	
-	public Currency pop(Currency obj) {
-		return null;
+	public Currency pop() {
+		
+		this.setCount(this.getCount() - 1);
+		LinkNode top = getEnd();
+		return top.getData();
 		
 	}
 	
 	public Currency peek() {
-		return null;
+		LinkNode top = getEnd();
+		return top.getData();
 		
 	}
 	
 	public void printStack() {
-		
+		for (int n = 0; n < this.getCount(); n++) {
+			//System.out.println("")
+		}
 	}
 }
